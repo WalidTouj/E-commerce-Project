ae6243ae22b6a62d644608cdd434bb17b4e5400b
Walid200044 <walidca572@gmail.com>
2021-03-28 15:42:30 +0100

    create E-commerce app using Router to navigate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./Pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const HatsPage = () => (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
