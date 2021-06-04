4ecfad94cdfbc0c9d9139904607f33d7082b6504
Walid200044 <walidca572@gmail.com>
2021-06-04 19:15:54 +0100

    broken the homepage to small component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import HomePage from "./Pages/homepage/homepage.component";
+import ShopPage from "./Pages/Shop/Shop.component";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const HatsPage = () => (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
